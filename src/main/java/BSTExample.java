addd9cf1114d04b83f6b5dec44e3943a25ec06e3
sanjeev <sanjeevmaths1729@gmail.com>
2023-12-28 16:43:28 +0530

    Displayed the same tree using INorder \Traversal

--- src/main/java/BSTExample.java
+++ src/main/java/BSTExample.java
@@ -5,6 +5,16 @@ public class BSTExample {
         bst.addNode(56);
         bst.addNode(30);
         bst.addNode(70);
+        bst.addNode(22);
+        bst.addNode(40);
+        bst.addNode(11);
+        bst.addNode(3);
+        bst.addNode(16);
+        bst.addNode(60);
+        bst.addNode(95);
+        bst.addNode(65);
+        bst.addNode(63);
+        bst.addNode(67);
 
         bst.displayTree();
     }
